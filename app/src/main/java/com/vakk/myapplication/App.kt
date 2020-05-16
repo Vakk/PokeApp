b2b5cd73d5f40b7847fa9863209e1310de9fb708
Valerii Kotsulym <valerii.kotsulym@magnise.com>
2020-05-16 18:10:30 +0300

    Base implementation for gradle modules.

---
+++ app/src/main/java/com/vakk/myapplication/App.kt
@@ -0,0 +1,16 @@
+package com.vakk.myapplication
+
+import com.vakk.myapplication.di.DaggerAppComponent
+import com.vakk.starter.BaseDaggerApplication
+
+class App : BaseDaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        DaggerAppComponent
+            .factory()
+            .create(this).apply {
+                (this)
+            }
+    }
+}
