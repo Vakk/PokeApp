aafbe3732513a97ab7a3d0c326e21c983db81453
Valerii Kotsulym <valerii.kotsulym@magnise.com>
2020-05-16 17:37:12 +0300

    Implementation for starter base components. Gradle for starter fixes.

--- starter/src/main/java/com/vakk/starter/ui/BaseFragment.kt
+++ starter/src/main/java/com/vakk/starter/ui/BaseFragment.kt
@@ -1,23 +1,23 @@
 package com.vakk.starter.ui
 
-import android.os.Bundle
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import dagger.android.support.DaggerAppCompatActivity
+import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
 /**
- * This activity represents regular flow with ViewModel and ViewModelProvider.Factory provided by dagger 2.
+ * This fragment base class represents regular flow with ViewModel and ViewModelProvider.Factory provided by dagger 2.
  */
-abstract class BaseActivity<VM : ViewModel>(
+abstract class BaseFragment<VM : ViewModel>(
     protected open val viewModelClass: Class<VM>
-) : DaggerAppCompatActivity() {
+) : DaggerFragment() {
 
     @Inject
     lateinit var viewModelProviderFactory: ViewModelProvider.Factory
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
         initializeViewModel(viewModelClass)
     }
 
