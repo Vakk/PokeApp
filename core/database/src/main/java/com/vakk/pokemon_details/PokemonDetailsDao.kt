21b8c2e1ca9201334deb1d451d089060ee999276
Valerii Kotsulym <valerii.kotsulym@magnise.com>
2020-05-18 01:42:18 +0300

    Update for DB. Update for data layer.

--- core/database/src/main/java/com/vakk/pokemon_details/PokemonDetailsDao.kt
+++ core/database/src/main/java/com/vakk/pokemon_details/PokemonDetailsDao.kt
@@ -1,2 +1,21 @@
 package com.vakk.pokemon_details
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface PokemonDetailsDao {
+    @Query("SELECT * FROM PokemonDetailsDto LIMIT :take OFFSET :skip")
+    fun getAll(skip: Int, take: Int): List<PokemonDetailsDto>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(entities: List<PokemonDetailsDto>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun replace(entity: PokemonDetailsDto)
+
+    @Query("DELETE FROM PokemonDetailsDto")
+    fun clear()
+}
