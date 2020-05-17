f58a8975e9023065dc61d0e344f28c8d182976cc
Valerii Kotsulym <valerii.kotsulym@magnise.com>
2020-05-17 21:19:26 +0300

    Fixes for get pokemons bean.

--- core/network/src/main/java/com/vakk/network/bean/GetPokemonsBean.kt
+++ core/network/src/main/java/com/vakk/network/bean/GetPokemonsBean.kt
@@ -3,6 +3,9 @@ package com.vakk.network.bean
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class GetPokemonsBean(
+    @JsonProperty("count") val count: Int,
+    @JsonProperty("next") val next: String?,
+    @JsonProperty("previous") val previous: String?,
     @JsonProperty("results")
     val results: List<ProtoInfoBean>
 )
